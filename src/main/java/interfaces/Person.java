c22e64c4f8b6af35651bc52c99188327b9c8e878
Misha Mosi <mihmosi@gmail.com>
2021-06-16 18:45:08 +0300

    create new class

--- src/main/java/interfaces/Person.java
+++ src/main/java/interfaces/Person.java
@@ -1,17 +1,18 @@
 package interfaces;
 
-public class Person implements Info{
+public class Person implements Info {
     String name;
 
-    public Person(String name){
+    public Person(String name) {
         this.name = name;
     }
-    public void sayHello(){
+
+    public void sayHello() {
         System.out.println("Hello!");
     }
 
     @Override
     public void showInfo() {
-        System.out.println("My name is "+this.name);
+        System.out.println("My name is " + this.name);
     }
 }
