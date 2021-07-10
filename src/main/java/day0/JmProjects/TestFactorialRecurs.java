0d7f5714fa63e0fb826d9245f14f7a1f92c187c4
mihmosi <mihmosi@gmail.com>
2021-07-10 11:07:04 +0300

    to fix recursia

--- src/main/java/day0/JmProjects/TestFactorialRecurs.java
+++ src/main/java/day0/JmProjects/TestFactorialRecurs.java
@@ -4,12 +4,12 @@ import java.math.BigInteger;
 
 public class TestFactorialRecurs {
     public static void main(String[] args) {
-        System.out.println(factorial(3));
+        System.out.println(factorial(0));
     }
 
     public static BigInteger factorial(int value) {
         BigInteger result = BigInteger.ONE;
-        if (value == 1) {
+        if (value == 1 || value == 0) {
             return result;
         }
         result = BigInteger.valueOf(value).multiply(factorial(value - 1));
